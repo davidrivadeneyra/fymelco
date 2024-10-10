4bc96b241c6329baa5a02439f72d13390a009a79
David Rivadeneyra <david.rivadeneyra.c@gmail.com>
2024-10-10 18:40:48 -0500

    pagina de producto unico

---
+++ select-img.js
@@ -0,0 +1,12 @@
+function selectImage(imageSrc) {
+
+  const currentSelected = document.querySelector('.product-thumbnails-item .selected');
+  if (currentSelected) {
+      currentSelected.classList.remove('selected');
+  }
+  
+  const newSelected = document.querySelector(`img[src='${imageSrc}']`);
+  newSelected.classList.add('selected');
+  
+    document.getElementById('selected-image').src = imageSrc;
+  }
